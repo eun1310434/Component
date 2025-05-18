e86827259861559514816369a5d2553d728daf5c
EUN-TAEK JEONG <eun1310434@naver.com>
2025-05-18 14:21:46 +0200

    Add SizeUtils

---
+++ ui-component/src/main/java/com/euntaek/uicomponent/utils/SizeUtils.kt
@@ -0,0 +1,14 @@
+package com.euntaek.uicomponent.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+
+@Stable
+private fun Dp.toPx(density: Density): Int = with(density) { this@toPx.roundToPx() }
+
+@Stable
+@Composable
+fun Dp.toPx(): Int = toPx(LocalDensity.current)
